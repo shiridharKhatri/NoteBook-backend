59eada1ab09cf437b53b6a3749113933cd8e4e7c
Shiridhar khatri <khatrishiridhar@gmail.com>
2023-01-21 14:40:51 +0545

    changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const path  = require('path');
 connectToServer();
 app.use(cors());
 app.use(express.json());
@@ -10,12 +11,11 @@ app.use("/auth", require("./routes/users"));
 app.use("/notes", require("./routes/notes"));
 app.use("/photo", require("./routes/avatar"));
 app.use("/images", express.static("./images"));
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 app.listen(PORT, () => {
   console.log(`Connected to port ${PORT}`);
